2c153b5e453d26f745fb2d694fc1be0cd4f6bcd7
kouya0911 <kouyaosa0911@gmail.com>
2025-08-04 21:07:06 +0900

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Todo } from "./Todo";
+
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <Todo />
+  </React.StrictMode>
+);
